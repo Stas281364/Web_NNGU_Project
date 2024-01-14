ab4ddd12406deb21e7f3e6cffe6cf95b0f1def6f
NastinNN <nastya.mannova@gmail.com>
2024-01-14 15:18:52 +0300

    Правки старого + новый файл со слайдером

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// topSlider
+const topSlider = new Swiper('.slider', {
+    loop: true,
+    spaceBetween: 20,
+  
+    // If we need pagination
+    pagination: {
+      el: '.slider__nav',
+      clickable: true
+    }
+});
