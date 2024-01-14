2356bb7a969b980b1e02cdb3c2fe0608af9366b2
NastinNN <nastya.mannova@gmail.com>
2024-01-14 16:39:32 +0300

    Добавила автоплей слайдера

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
 // topSlider
 const topSlider = new Swiper('.slider', {
     loop: true,
-    spaceBetween: 20,
-  
+    spaceBetween: 100,
+
+    autoplay: {
+      delay: 5000,
+     },
+     speed: 1500,
+
     // If we need pagination
     pagination: {
       el: '.slider__nav',
       clickable: true
-    }
+    },
+
+    on: {
+      init() {
+        this.el.addEventListener('mouseenter', () => {
+          this.autoplay.stop();
+        });
+  
+        this.el.addEventListener('mouseleave', () => {
+          this.autoplay.start();
+        });
+      }
+    },
 });
